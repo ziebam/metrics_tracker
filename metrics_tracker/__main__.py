a443a292322d0662cf98e8468329fe1b3d6178d9
ziebam <ziebamichal@tutanota.com>
2020-08-28 18:35:47 +0200

    Revamp the arguments
     - Added flags to some of the arguments. As "--reset", "--remove"
       and "--delete" are more destructive than others, they still
       have to be called by the full name.
     - Slightly simplified the parsing logic for the possible
       abstraction of it in the future.

--- metrics_tracker/__main__.py
+++ metrics_tracker/__main__.py
@@ -24,22 +24,24 @@ def main():
 
     parser = argparse.ArgumentParser(prog="Metrics Tracker")
 
-    if len(sys.argv) == 1:
-        sys.exit(parser.print_help())
-
-    parser.add_argument("--create", nargs=1, metavar="metric")
-    parser.add_argument("--add", nargs=2, type=int, metavar=("metric", "amount"))
-    parser.add_argument("--update", nargs=2, metavar=("index", "new_name"))
-    parser.add_argument("--reset", nargs=1, type=int, metavar="metric")
-    parser.add_argument("--remove", nargs=1, type=int, metavar="metric")
+    parser.add_argument("-c", "--create", metavar="metric")
+    parser.add_argument(
+        "-a", "--add", nargs=2, type=int, metavar=("metric_id", "amount")
+    )
+    parser.add_argument("-u", "--update", nargs=2, metavar=("metric_id", "new_name"))
+    parser.add_argument("--reset", type=int, metavar="metric_id")
+    parser.add_argument("--remove", type=int, metavar="metric_id")
     parser.add_argument("--delete", action="store_true")
     parser.add_argument("--list", action="store_true")
+
+    if len(sys.argv) == 1:
+        sys.exit(parser.print_help())
 
     args = parser.parse_args()
 
     if args.create:
         metric_id = len(metrics) + 1
-        metric = args.create[0]
+        metric = args.create
         metrics.append([metric_id, metric, 0])
 
         clear_screen()
@@ -47,21 +49,18 @@ def main():
         print_data(metrics)
 
     if args.add:
-        metric_id, to_add = args.add
-        metrics[metric_id - 1][2] += to_add
+        index, to_add = (args.add[0] - 1, args.add[1])
+        metrics[index][2] += to_add
 
         clear_screen()
-        print(
-            f'Added {to_add} to the metric "{metrics[metric_id - 1][1]}" succesfully.'
-        )
+        print(f'Added {to_add} to the metric "{metrics[index][1]}" succesfully.')
         print_data(metrics)
 
     if args.update:
-        index, new_name = args.update
-        index = int(index)
-        old_name = metrics[index - 1][1]
+        index, new_name = (int(args.update[0]) - 1, args.update[1])
+        old_name = metrics[index][1]
 
-        metrics[index - 1][1] = new_name
+        metrics[index][1] = new_name
 
         clear_screen()
         print(f'Updated name of metric "{old_name}" to "{new_name}" succesfully.')
@@ -69,24 +68,24 @@ def main():
 
     if args.reset:
         if decision_confirmed():
-            index = args.reset[0]
-            metrics[index - 1][2] = 0
+            index = args.reset - 1
+            metrics[index][2] = 0
 
             clear_screen()
-            print(f'Reset metric "{metrics[index - 1][1]}" to 0 succesfully.')
+            print(f'Reset metric "{metrics[index][1]}" to 0 succesfully.')
             print_data(metrics)
         else:
             sys.exit("Operation cancelled. Aborting.")
 
     if args.remove:
         if decision_confirmed():
-            index = args.remove[0]
-            name = metrics[index - 1][1]
+            index = args.remove - 1
+            name = metrics[index][1]
 
-            for idx, _ in enumerate(metrics[index:]):
-                metrics[idx][0] -= 1
+            for metric in metrics[index + 1 :]:
+                metric[0] -= 1
 
-            del metrics[index - 1]
+            del metrics[index]
 
             clear_screen()
             print(f'Removed metric "{name}" succesfully.')
