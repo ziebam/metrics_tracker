97ca942d7878cadbf37da605d6ed597da0878878
ziebam <ziebamichal@tutanota.com>
2020-08-29 08:26:08 +0200

    Add docstrings for action handlers

--- metrics_tracker/action_handlers.py
+++ metrics_tracker/action_handlers.py
@@ -1,9 +1,18 @@
+"""Action handlers for the parsed data."""
+
 import sys
 
 from .pprinting_utils import clear_screen, decision_confirmed, print_data
 
 
 def create_metric(metric_name, metrics):
+    """Creates the metric.
+
+    Args:
+        metric_name: A name for the new metric.
+        metrics: A list of all metrics.
+    """
+
     metric_id = len(metrics) + 1
     metrics.append([metric_id, metric_name, 0])
 
@@ -13,6 +22,14 @@ def create_metric(metric_name, metrics):
 
 
 def add_to_metric(metric_id, amount_to_add, metrics):
+    """Adds a value to the metric.
+
+    Args:
+        metric_id: ID of the metric. The metrics are one-indexed.
+        amount_to_add: A value to add to the metric.
+        metrics: A list of all metrics.
+    """
+
     index = metric_id - 1
     metrics[index][2] += amount_to_add
 
@@ -22,6 +39,14 @@ def add_to_metric(metric_id, amount_to_add, metrics):
 
 
 def update_metric(metric_id, new_metric_name, metrics):
+    """Updates the metric's name.
+
+    Args:
+        metric_id: ID of the metric. The metrics are one-indexed.
+        new_metric_name: A name to set the old name to.
+        metrics: A list of all metrics.
+    """
+
     index = int(metric_id) - 1
     old_name = metrics[index][1]
 
@@ -35,6 +60,13 @@ def update_metric(metric_id, new_metric_name, metrics):
 
 
 def reset_metric(metric_id, metrics):
+    """Resets a metric's value to 0.
+
+    Args:
+        metric_id: ID of the metric. The metrics are one-indexed.
+        metrics: A list of all metrics.
+    """
+
     if decision_confirmed():
         index = metric_id - 1
         metrics[index][2] = 0
@@ -47,6 +79,13 @@ def reset_metric(metric_id, metrics):
 
 
 def remove_metric(metric_id, metrics):
+    """Removes a metric.
+
+    Args:
+        metric_id: ID of the metric. The metrics are one-indexed.
+        metrics: A list of all metrics.
+    """
+
     if decision_confirmed():
         index = metric_id - 1
         metric_name = metrics[index][1]
@@ -64,6 +103,12 @@ def remove_metric(metric_id, metrics):
 
 
 def delete_metrics(metrics):
+    """Deletes all metrics.
+
+    Args:
+        metrics: A list of all metrics.
+    """
+
     if decision_confirmed():
         metrics.clear()
         clear_screen()
@@ -73,6 +118,12 @@ def delete_metrics(metrics):
 
 
 def list_metrics(metrics):
+    """Pretty-prints the metrics to the terminal.
+
+    Args:
+        metrics: A list of all metrics.
+    """
+
     if metrics:
         clear_screen()
         print_data(metrics)
